7e174077d4a04dfd124e027d57a1f1bdcb236800
G-sel <gselloro46@gmail.com>
2023-08-16 12:44:34 +0800

    change resume link

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -7,7 +7,7 @@
     <h1 class="text-light flex-start">This is &nbsp;<p class="text-warning name"> G-sel Loro</p></h1>
     <h1 class="text-light flex-start font">I'm a &nbsp;<p class="text-warning name"> Web Developer</p></h1>
     <div class="button flex-start">
-    <button class="button rounded-top bg-warning "><a href="https://drive.google.com/file/d/1Jxkxvz7Dg-FR8-ve1WyN3-xPkzw7ig99/view?usp=sharing" target="_blank" class="text-decoration-none text-light">Download Resume</a></button>
+    <button class="button rounded-top bg-warning "><a href="https://drive.google.com/file/d/1r_beXWXykl2ea2V1h5UiGMwy-ESsy6ed/view?usp=sharing" target="_blank" class="text-decoration-none text-light">Download Resume</a></button>
 </div>
 </div>
 </body>
