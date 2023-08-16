0abd368faba92a6207498ca02e30a452cadd9b54
G-sel <gselloro46@gmail.com>
2023-08-16 12:24:44 +0800

    font-style

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -3,12 +3,11 @@
     <body>
 <div class="container">
 
-    <p class="flex-start">Let's create your website</p>
-    <h1 class="text-light flex-start">This is &nbsp;<p class="text-warning"> G-sel Loro</p></h1>
-    <h1 class="text-light flex-start font">I'm a &nbsp;<p class="text-warning"> Web Developer</p></h1>
+    <p class="flex-start text-light">Let's create your website</p>
+    <h1 class="text-light flex-start">This is &nbsp;<p class="text-warning name"> G-sel Loro</p></h1>
+    <h1 class="text-light flex-start font">I'm a &nbsp;<p class="text-warning name"> Web Developer</p></h1>
     <div class="button flex-start">
     <button class="button rounded-top bg-warning "><a href="https://drive.google.com/file/d/1Jxkxvz7Dg-FR8-ve1WyN3-xPkzw7ig99/view?usp=sharing" target="_blank" class="text-decoration-none text-light">Download Resume</a></button>
-    <!-- <button class="button download "><a href="./../assets/LORO_GSEL_RESUME.pdf" class="text-download text-warning" download>Download My CV</a></button> -->
 </div>
 </div>
 </body>
@@ -21,13 +20,15 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,300;0,400;1,300;1,400&display=swap');
-
+@import url('https://fonts.googleapis.com/css2?family=Barriecito&display=swap');
 body{
     height:90vh;
     background-color: #212529;
     font-family: 'Kanit', sans-serif;
+}
+
+.name{
+    font-family: 'Barriecito', cursive;
 }
 
 
