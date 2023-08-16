f3bc0272ca44473c133bc4b872724fc4ad05666e
G-sel <gselloro46@gmail.com>
2023-08-16 12:16:36 +0800

    landing page color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- new nav bar -->
-<nav class="navbar navbar-expand-lg bg-#000">
+<nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand logo text-warning" href="#">GL.</a>
     <!-- <a class="navbar-brand" href="#">Navbar w/ text</a> -->
